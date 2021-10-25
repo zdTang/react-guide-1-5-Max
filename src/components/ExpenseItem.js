59e7e7538c3d7c26e0277d9aef51890805a67a5d
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-10-25 15:40:21 -0400

    Outputting Dynamic Data and working with Expressions in JSX
    In JSX
    we can do lots of operation, such as call function and  mathematics

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css'   // this used to make React to be aware of the css file
 function ExpenseItem() {
     //short-cut:  shift + alt + f => manage format
+    const expenseDate = new Date(2021,10,10)
+    const expenseTitle = "Car Insurance"
+    const expenseAmount = 294.67
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseDate.toString()}</div>
       <div className="expense__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
