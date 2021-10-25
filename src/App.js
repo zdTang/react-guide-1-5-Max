ededccf3d854cfea05d8c268d861fd91cabc17bc
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-10-25 16:56:12 -0400

    Split Date Component
    1, Split Date component out of the ExpenseItem
    2, Use  [ ] data to loop many Components. it should not put into the Return( )
    3. [ ] loop need a key property or Error will occur
    4, Each component will follow a CSS file

--- src/App.js
+++ src/App.js
@@ -2,37 +2,41 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const DUMMY_EXPENSES = [
     {
-      id: 'e1',
-      title: 'Toilet Paper',
+      id: "e1",
+      title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: 'e3',
-      title: 'Car Insurance',
+      id: "e3",
+      title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
+      id: "e4",
+      title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
   ];
-  
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      DUMMY_EXPENSES.forEach(element => {
-        <ExpenseItem date={element.date}
-        title={element.title} 
-        amount={element.amount}/>
-      });
-      
-    </div>
-  );
+
+  let res = [];
+
+  DUMMY_EXPENSES.forEach((element, index) => {
+    let singleItem = (
+      <ExpenseItem
+        key={index}
+        date={element.date}
+        title={element.title}
+        amount={element.amount}
+      />
+    );
+    res.push(singleItem);
+  });
+
+  return <div>{res}</div>;
 }
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,11 @@
 import './ExpenseItem.css'   // this used to make React to be aware of the css file
+import ExpenseDate from './ExpenseDate';
 function ExpenseItem(props) {
     //short-cut:  shift + alt + f => manage format
-const month=props.date.toLocaleString('en-US',{month:'long'})
-const day=props.date.toLocaleString('en-US',{day:'2-digit'})
-const year=props.date.getFullYear()
+
   return (
     <div className="expense-item">
-        <div>
-            <div>{month}</div>
-            <div>{day}</div>
-            <div>{year}</div>
-        </div>
+       <ExpenseDate date={props.date}/>
       <div className="expense__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
