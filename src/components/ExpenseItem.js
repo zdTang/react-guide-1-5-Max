c12b9211fd1911931b884f2c8c88de11e8ee4419
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-10-25 16:25:22 -0400

    Deal with the Date with JS logic
    use 'props' to pass data to a component
    use { } to inject the coming data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,19 @@
 import './ExpenseItem.css'   // this used to make React to be aware of the css file
-function ExpenseItem() {
+function ExpenseItem(props) {
     //short-cut:  shift + alt + f => manage format
-    const expenseDate = new Date(2021,10,10)
-    const expenseTitle = "Car Insurance"
-    const expenseAmount = 294.67
+const month=props.date.toLocaleString('en-US',{month:'long'})
+const day=props.date.toLocaleString('en-US',{day:'2-digit'})
+const year=props.date.getFullYear()
   return (
     <div className="expense-item">
-      <div>{expenseDate.toString()}</div>
+        <div>
+            <div>{month}</div>
+            <div>{day}</div>
+            <div>{year}</div>
+        </div>
       <div className="expense__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
