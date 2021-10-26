23a085b8f4380e1e1abda0b860db9bc561d36920
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-10-26 18:22:57 -0400

    Revert "change state by useState()"
    This reverts commit 82022177d99bc96862dc81667d510e7f6bb11907.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,18 @@
 import './ExpenseItem.css'   // this used to make React to be aware of the css file
 import ExpenseDate from './ExpenseDate';
 import Card from './Card'
-import React, {useState} from 'react'
 const ExpenseItem=(props)=> {
-  // react hooks
-  const [title,setTitle]=useState(props.title)
   
   // create an event handler
   const clickHandler =()=>{
-    setTitle("newValue")
-    console.log(title)
+    alert("clicked")
   }
 
   return (
     <Card className="expense-item">
        <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       {/* add event to a button */}
