c18049d78dc722391384a4a93292fdf950bd784b
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-10-26 16:11:24 -0400

    fix style bug.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,9 +7,9 @@ const ExpenseItem=(props)=> {
   return (
     <Card className="expense-item">
        <ExpenseDate date={props.date}/>
-      <div className="expense__description">
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
   );
