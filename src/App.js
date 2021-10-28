97cbb1878e9029f26d10ed03f1461a6249b5bcd4
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-10-28 11:27:58 -0400

    05-003 Using Stateful Lists
    update list
    render the list dynamically
    in the expenseItem, if display those value with useState(),  when the List is updated, new List cannot be display on time

--- src/App.js
+++ src/App.js
@@ -1,53 +1,48 @@
 //import ExpenseItem from "./components/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses"
 import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
+
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 const App=()=> {
-  const DUMMY_EXPENSES = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
 
-  // let res = [];
+  const [expenses,setExpenses]=useState(DUMMY_EXPENSES)
+  
 
-  // DUMMY_EXPENSES.forEach((element, index) => {
-  //   let singleItem = (
-  //     <ExpenseItem
-  //       key={index}
-  //       date={element.date}
-  //       title={element.title}
-  //       amount={element.amount}
-  //     />
-  //   );
-  //   res.push(singleItem);
-  //
+  
 
-  //return <div>{res}</div>;
-
-  const addExpenseHandler=(data)=>{
-    console.log("App: ",data)
+  const addExpenseHandler=(expense)=>{
+    //setExpenses([expense,...expenses])
+    setExpenses(prevExpenses=>[expense,...prevExpenses])
   }
+
+  console.log(expenses)
   return <div>
     {/* this 'onData' will be called by Son component */}
     {/* Once called by Son, it will call it's dealData */}
     <NewExpense onAddExpense={addExpenseHandler}/>    
-    <Expenses items={DUMMY_EXPENSES}/>
+    <Expenses items={expenses}/>
   </div>
 }
 
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,24 @@
 import './ExpenseItem.css'   // this used to make React to be aware of the css file
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card'
-import React, {useState} from 'react'
 const ExpenseItem=(props)=> {
   // react hooks
   // first arg: current state
   // second arg: function to update current state
-  const [title,setTitle]=useState(props.title)
+  // when props updated, if using the following lind
+  // the update will not be displayed on time
+  //const [title,setTitle]=useState(props.title)
 
-
+console.log(props)
 
 
   return (
     <Card className="expense-item">
        <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
+        {/* if using useState here, once props update, new data cannot be displayed */}
+        {/* <h2>{title}</h2> */}
         <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
