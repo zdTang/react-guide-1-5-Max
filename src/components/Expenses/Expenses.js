9ef522a3c2f5682f5db85435912c96ff2a7b9056
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-10-28 09:41:24 -0400

    Add filter
    Component's props can be used to pass data down, and up as well

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,27 @@
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
+
+
+
+
 const Expenses=(props)=> {
+
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    console.log(selectedYear)
+    setFilteredYear(selectedYear);
+  };
+
+
   return (
     <Card className="expenses">
+      {/*ExpensesFilter has two props, one is pass down, another one is pass up  */}
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
