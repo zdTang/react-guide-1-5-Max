2048f1216b392d021633a8c29c789bad0248cd80
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-10-28 17:22:04 -0400

    05-006 Adding Conditional Return Statements
    extract  some part to make a single component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
-import './ExpenseItem.css'   // this used to make React to be aware of the css file
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card'
-const ExpenseItem=(props)=> {
+import "./ExpenseItem.css"; // this used to make React to be aware of the css file
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+const ExpenseItem = (props) => {
   // react hooks
   // first arg: current state
   // second arg: function to update current state
@@ -9,21 +9,21 @@ const ExpenseItem=(props)=> {
   // the update will not be displayed on time
   //const [title,setTitle]=useState(props.title)
 
-console.log(props)
-
-
+  console.log(props);
 
   return (
-    <Card className="expense-item">
-       <ExpenseDate date={props.date}/>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        {/* if using useState here, once props update, new data cannot be displayed */}
-        {/* <h2>{title}</h2> */}
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          {/* if using useState here, once props update, new data cannot be displayed */}
+          {/* <h2>{title}</h2> */}
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 
@@ -20,47 +20,15 @@ const Expenses=(props)=> {
   const filteredExpenses=props.items.filter(expense=>{
     return expense.date.getFullYear().toString()===filteredYear
   })
-  /* approach 3: Define a default value and cover it based on condition*/
-  let expensesContent=<p>No Expense found.</p>                    // define default value
-  if(filteredExpenses.length>0){
-    expensesContent=filteredExpenses.map((expense)=> <ExpenseItem // over-written the default
-    title={expense.title}
-    amount={expense.amount}
-    date={expense.date}
-    key={expense.id}  
-  />)
-  }
+ 
+
 
 
   return (
     <Card className="expenses">
       {/*ExpensesFilter has two props, one is pass down, another one is pass up  */}
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {/* must in the {} as it is a JS expression other than JSX */}
-
-      {/* approach 1   use trinary expression*/}
-      {/* {
-        filteredExpenses.length===0
-        ?<p>No Expense found.</p>
-        :(filteredExpenses.map((expense)=> <ExpenseItem
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-        key={expense.id}  //  add a key is very important or the react will render the whole array
-      />))
-      } */}
-
-      {/* approach 2   use JS trick: if the left side of && is true, will return the right side of && */}
-      {/* 
-      {filteredExpenses.length===0 && <p>No Expense found.</p>}
-      {filteredExpenses.length>0 && (filteredExpenses.map((expense)=> <ExpenseItem
-      title={expense.title}
-      amount={expense.amount}
-      date={expense.date}
-      key={expense.id}  //  add a key is very important or the react will render the whole array
-    />))} */}
-
-    {expensesContent}   {/* approach 3 */}
+      <ExpensesList items={filteredExpenses}/>
     </Card>
   );
 }
